68434a8567454d0a3e1e67409e1826a0b07c97e1
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-05 23:20:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(listofnames)
   listoftags = Array.new()
   length = listofnames.length 
   start = 0 
-  while start < length do 
-    
+  while start < length do
+    namepull = listofnames.index(start)
+    phrase = "Hello, my name is #{namepull}."
     listoftags.push(phrase)
+    start += 1 
+  end
+end
